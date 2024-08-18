6fb6818451d65b4a24a7d460553262522ef834da
viking72 <100124541+viveksingh0072@users.noreply.github.com>
2024-08-18 10:43:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            /> 
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
